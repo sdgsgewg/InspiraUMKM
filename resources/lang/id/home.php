0a6312f5045444288250070410798b7764e968b4
Vincent Oei <oei.vincent20@gmail.com>
2024-12-18 20:09:06 +0700

    add new sentences

--- resources/lang/id/home.php
+++ resources/lang/id/home.php
@@ -2,4 +2,6 @@
 
 return [
     'special_offers' => 'Penawaran Khusus',
+    'headline' => 'Tingkatkan Branding Bisnismu - Pesan Sekarang!',
+    'title' => 'Solusi Terpadu untuk Kebutuhan Branding UMKM',
 ];
