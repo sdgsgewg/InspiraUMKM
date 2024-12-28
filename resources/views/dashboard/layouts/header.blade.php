517aac0a134dc2a09fa38b618c69a6f0757a10d6
Jessen <jessen123ptk@gmail.com>
2024-12-28 11:55:17 +0700

    Production fix

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -3,28 +3,11 @@
 
     <ul class="navbar-nav flex-row d-md-none">
         <li class="nav-item text-nowrap">
-            <!-- Search toggle button (visible on small screens) -->
-            {{-- <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch"
-                aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
-                <svg class="bi">
-                    <use xlink:href="#search" />
-                </svg>
-            </button> --}}
-        </li>
-        <li class="nav-item text-nowrap">
-            <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas"
+            <button class="nav-link d-inline-flex py-0 px-3 text-white" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <svg class="bi">
-                    <use xlink:href="#list" />
-                </svg>
+                <i class="bi bi-list fs-4"></i>
             </button>
         </li>
     </ul>
-
-    <!-- Search box -->
-    <!-- Hidden on small screens, displayed on medium and larger screens by default -->
-    {{-- <div id="navbarSearch" class="navbar-search w-100 collapse d-md-block">
-        <input class="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-    </div> --}}
 </header>
